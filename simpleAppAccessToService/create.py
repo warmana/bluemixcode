56893f4531943f9cfc1770014d11e7815d888cac
Adrian Warman <warmana@gmail.com>
2016-12-15 09:57:16 +0000

    Polish code, for publication.

--- simpleAppAccessToService/create.py
+++ simpleAppAccessToService/create.py
@@ -9,8 +9,11 @@ serviceUsername = "353466e8-47eb-45ce-b125-4a4e1b5a4f7e-bluemix"
 servicePassword = "49c0c343d225623956157d94b25d574586f26d1211e8e589646b4713d5de4801"
 serviceURL = "https://353466e8-47eb-45ce-b125-4a4e1b5a4f7e-bluemix.cloudant.com"
 
+# This is the name of the database we are working with.
 databaseName = "databasedemo"
 
+# This is a simple collection of data,
+# to store within the database.
 sampleData = [
     [1, "one", "boiling", 100],
     [2, "two", "hot", 40],
@@ -19,39 +22,36 @@ sampleData = [
     [5, "five", "freezing", 0]
 ]
 
-# Use Cloudant to create a Cloudant client using account
-# client = Cloudant(username, password, account=account)
-# or using url
+# Start the demo.
+print "===\n"
+
+# Use the Cloudant library to create a Cloudant client.
 client = Cloudant(serviceUsername, servicePassword, url=serviceURL)
 
 # Connect to the server
 client.connect()
-
-# Perform client tasks...
-session = client.session()
-print 'Username: {0}'.format(session['userCtx']['name'])
-print 'Databases: {0}'.format(client.all_dbs())
-
-# Delete a pre-existing instance of the test database.
-try :
-    client.delete_database(databaseName)
-except CloudantException:
-    print "{0} does not exist.".format(databaseName)
 
 # Create a fresh instance of the database.
 myDatabaseDemo = client.create_database(databaseName)
 
 # Check that the database now exists.
 if myDatabaseDemo.exists():
-    print "{0} successfully created.".format(databaseName)
+    print "'{0}' successfully created.\n".format(databaseName)
+
+# Space out the results.
+print "----\n"
 
 # Create documents using the sample data.
+# Go through each row in the array
 for document in sampleData:
+    # Retrieve the fields in each row.
     number = document[0]
     name = document[1]
     description = document[2]
     temperature = document[3]
 
+    # Create a JSON document that represents
+    # all the data in the row.
     jsonDocument = {
         "numberField": number,
         "nameField": name,
@@ -59,22 +59,32 @@ for document in sampleData:
         "temperatureField": temperature
     }
 
-    # Create a document using the Database API
+    # Create a document using the Database API.
     newDocument = myDatabaseDemo.create_document(jsonDocument)
 
-    # Check that the document exists in the database
+    # Check that the document exists in the database.
     if newDocument.exists():
-        print "Document {0} successfully created.".format(number)
+        print "Document '{0}' successfully created.".format(number)
 
-# Simple and minimal retrieval of documents.
+# Space out the results.
+print "----\n"
+
+# Simple and minimal retrieval of the first
+# document in the database.
 result_collection = Result(myDatabaseDemo.all_docs)
-print "Retrieved minimal document: {0}".format(result_collection[0])
+print "Retrieved minimal document:\n{0}\n".format(result_collection[0])
 
-# Simple and full retrieval of documents.
+# Simple and full retrieval of the first
+# document in the database.
 result_collection = Result(myDatabaseDemo.all_docs, include_docs=True)
-print "Retrieved full document: {0}".format(result_collection[0])
+print "Retrieved full document:\n{0}\n".format(result_collection[0])
 
-print "\n\n\n"
+# Space out the results.
+print "----\n"
+
+# Use a Cloudant API endpoint to retrieve
+# all the documents in the database,
+# including their content.
 
 # Define the end point and parameters
 end_point = '{0}/{1}'.format(serviceURL, databaseName + "/_all_docs")
@@ -84,9 +94,27 @@ params = {'include_docs': 'true'}
 response = client.r_session.get(end_point, params=params)
 
 # Display the response content
-print response.json()
+print "{0}\n".format(response.json())
+
+# Space out the results.
+print "----\n"
+
+# All done.
+# Time to tidy up.
+
+# Delete the test database.
+try :
+    client.delete_database(databaseName)
+except CloudantException:
+    print "There was a problem deleting '{0}'.\n".format(databaseName)
+else:
+    print "'{0}' successfully deleted.\n".format(databaseName)
 
 # Disconnect from the server
 client.disconnect()
 
+# Finish the demo.
+print "===\n"
+
+# Say good-bye.
 exit()
